de00e5292ac3607c8a7cbb0b181500b3e2ceb05e
Hasan Atay <hasan@dkoda.xyz>
2023-02-01 20:28:35 -0500

    Updates

--- contracts/conduit/WasabiConduit.sol
+++ contracts/conduit/WasabiConduit.sol
@@ -61,43 +61,6 @@ contract WasabiConduit is Ownable, IERC721Receiver {
         IERC721(_nft).safeTransferFrom(address(this), _target, _tokenId);
     }
 
-    // constructor() {
-    //     maxNumOptions = 1;
-    // }
-
-    // uint256 private maxNumOptions;
-
-    // struct MultiOptionRequest {
-    //     WasabiStructs.OptionRequest request;
-    //     bytes _signature;
-    // }
-
-    // function buyOptions(MultiOptionRequest[] calldata _requests) external payable {
-    //     uint256 numRequests = _requests.length;
-
-    //     require(numRequests > 0, "Need to provide at least one request");
-    //     require(numRequests <= maxNumOptions, "Cannot buy that many options at once");
-
-    //     for (uint256 i; i < numRequests; ) {
-    //         MultiOptionRequest memory request = _requests[i];
-    //         IWasabiPool pool = IWasabiPool(request.request.poolAddress);
-
-    //         if (pool.getCommodityAddress() != address(0)) {
-    //             IERC20 erc20 = IERC20(pool.getCommodityAddress());
-    //             erc20.transferFrom(_msgSender(), address(this), request.request.premium);
-    //             erc20.approve(request.request.poolAddress, request.request.premium);
-    //         }
-
-    //         // TODO: Use option id returned from write option once its changed
-    //         pool.writeOption(request.request, request._signature);
-    //         option.safeTransferFrom(address(this), _msgSender(), pool.getOptionIds()[pool.getOptionIds().length - 1]);
-
-    //         unchecked {
-    //             ++i;
-    //         }
-    //     }
-    // }
-
     function setOption(WasabiOption _option) external onlyOwner {
         option = _option;
     }
---
+++ modules/as_truffle-assertions.d.ts
@@ -0,0 +1,7 @@
+// declare module "truffle-assertions" {
+//     function reverts(a: any, b: any, c: any): Function;
+//     function eventEmitted(a: any, b: any, c: any, d: any): Function;
+//     export {
+//         reverts, eventEmitted
+//     }
+// }

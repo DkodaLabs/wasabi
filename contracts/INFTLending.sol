f07cd9462face4d99ad0a110c12d5f1c2dd2bb44
wasabijk <jason@dkoda.xyz>
2023-06-14 12:25:02 -0500

    add repay function, add return value for borrow function

--- contracts/INFTLending.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
-
-/// @notice NFTLending Interface
-interface INFTLending {
-    /// @notice Borrow WETH from the protocol
-    /// @param inputs Encoded input parameters
-    function borrow(bytes calldata inputs) external;
-}

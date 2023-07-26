f2895395d0cfb7ec4362339183d62f69441f2dcd
wasabijk <jason@dkoda.xyz>
2023-07-26 10:40:26 -0500

    update x2y2 data types

--- contracts/lending/interfaces/x2y2/IXY3.sol
+++ contracts/lending/interfaces/x2y2/IXY3.sol
@@ -2,33 +2,52 @@
 pragma solidity 0.8.19;
 
 interface IXY3 {
+    struct Signature {
+        address signer;
+        bytes signature;
+    }
+
+    struct BrokerSignature {
+        address signer;
+        bytes signature;
+        uint32 expiry;
+    }
+
+    enum StatusType {
+        NOT_EXISTS,
+        NEW,
+        RESOLVED
+    }
+
     struct LoanDetail {
-        uint256 borrowAmount;
-        uint256 repayAmount;
-        uint256 nftTokenId;
-        address borrowAsset;
+        StatusType state;
+        uint64 reserved;
         uint32 loanDuration;
         uint16 adminShare;
         uint64 loanStart;
-        address nftAsset;
-        bool isCollection;
+        uint8 borrowAssetIndex;
+        uint32 nftAssetIndex;
+        uint112 borrowAmount;
+        uint112 repayAmount;
+        uint256 nftTokenId;
+    }
+
+    enum ItemType {
+        ERC721,
+        ERC1155
     }
 
     struct Offer {
+        ItemType itemType;
         uint256 borrowAmount;
         uint256 repayAmount;
         address nftAsset;
-        uint32 borrowDuration;
         address borrowAsset;
-        uint256 timestamp;
-        bytes extra;
-    }
-
-    struct Signature {
-        uint256 nonce;
-        uint256 expiry;
-        address signer;
-        bytes signature;
+        uint256 tokenId;
+        uint32 borrowDuration;
+        uint32 validUntil;
+        uint32 amount;
+        Signature signature;
     }
 
     struct CallData {
@@ -36,17 +55,19 @@ interface IXY3 {
         bytes4 selector;
         bytes data;
         uint256 referral;
+        address onBehalf;
+        bytes32[] proof;
     }
 
-    function loanDetails(uint32 _loanId) external view returns (LoanDetail memory);
+    function loanDetails(
+        uint32 _loanId
+    ) external view returns (LoanDetail memory);
 
     function borrow(
-        Offer calldata _offer,
+        Offer memory _offer,
         uint256 _nftId,
-        bool _isCollectionOffer,
-        Signature calldata _lenderSignature,
-        Signature calldata _brokerSignature,
-        CallData calldata _extraDeal
+        BrokerSignature memory _brokerSignature,
+        CallData memory _extraData
     ) external returns (uint32);
 
     function repay(uint32 _loanId) external;
